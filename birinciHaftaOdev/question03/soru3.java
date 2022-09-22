d9fe9a484225ec014e4217f74817b10f1042527b
yolkar62 <58560920+yolkar62@users.noreply.github.com>
2022-09-22 21:28:56 +0300

    Initial commit

---
+++ birinciHaftaOdev/question03/soru3.java
@@ -0,0 +1,23 @@
+
+/**
+ * 200 ile 500 arasındaki 3 ile tam bölünen sayılar dışındaki sayıların toplamını bulan program.
+ *
+ */
+
+package question03;
+
+public class soru3 {
+    public static void main(String[] args) {
+
+            int toplam = 0;
+
+            for(int i = 0; i < 5 ; i++){
+                if(i%3 == 0){
+                    continue;
+                }
+                toplam += i;
+            }System.out.println(toplam);
+
+        }
+
+}
