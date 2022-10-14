dd0d8015335511c5057b584516d1fcf90b06b110
yolkar62 <58560920+yolkar62@users.noreply.github.com>
2022-10-14 22:46:28 +0300

    Assignment3
    Assignment

---
+++ week3/E-Manav/src/entities/Fruit.java
@@ -0,0 +1,32 @@
+package entities;
+
+public class Fruit {
+
+        public static String fruitName;
+        public static int kilogram;
+        public static double price;
+
+        public Fruit(String fruitName, int kilogram, double price) {
+                this.fruitName = fruitName;
+                this.kilogram = kilogram;
+                this.price = price;
+        }
+
+        public String fruitName() {
+
+                return null;
+        }
+
+        public int kilogram() {
+
+                return 0;
+        }
+
+        public double price() {
+
+                return 0;
+        }
+
+
+
+}
